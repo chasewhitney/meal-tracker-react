5eac528441be52e609aacb41e751470236ca4ef5
Chase Whitney <ChaseEWhitney@gmail.com>
2019-02-14 17:18:18 -0600

    server-side api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,11 +5,13 @@ const request = require('request');
 
 module.exports = app => {
 
-  app.get('/api/instant', function(req, res){
-    console.log('req.query.searchQuery:', req.query.searchQuery);
-    const toQuery = req.query.searchQuery;
+  // GET INSTANT DROPDOWN LIST
+  app.get('/api/instant', (req, res) => {
+    const { searchQuery } = req.query;
+    console.log('searchQuery:', searchQuery);
+
     const options = {
-      url: 'https://trackapi.nutritionix.com/v2/search/instant?query=' + toQuery,
+      url: 'https://trackapi.nutritionix.com/v2/search/instant?detailed=true&query=' + searchQuery,
       headers: {
         'x-app-id': keys.nutriId,
         'x-app-key': keys.nutriKey
@@ -25,5 +27,62 @@ module.exports = app => {
       }
     });
   });
+
+  // GET SELECTED COMMON ITEM
+  app.get('/common', (req, res) => {
+  console.log('req.body:', req.body);
+
+  const {toQuery} = req.query;
+  const options = {
+    url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+    method: 'POST',
+    headers: {
+      'x-app-id': appId,
+      'x-app-key': apiKey
+    },
+    body: {
+      "query" : toQuery
+    },
+    json: true
+
+  };
+
+  request(options, (err, response, body) => {
+    if(err) {
+      console.log('error:', err);
+      res.sendStatus(500);
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+// GET SELECTED BRANDED ITEM
+app.get('/branded', (req, res) => {
+  console.log('req.query:', req.query);
+
+  const { toQuery } = req.query;
+  const options = {
+    url: 'https://trackapi.nutritionix.com/v2/search/item?nix_item_id=' + toQuery,
+    headers: {
+      'x-app-id': appId,
+      'x-app-key': apiKey
+    }
+  };
+
+  request(options, (err, response, body) => {
+    if(err) {
+      console.log('error:', err);
+      res.sendStatus(500);
+    } else {
+      res.send(JSON.parse(body));
+    }
+  });
+});
+
+app.get('/*', (req, res) => {
+  console.log('404 : ', req.params);
+  res.sendStatus(404);
+});
 
 }
