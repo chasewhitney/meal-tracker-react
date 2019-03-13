64bc520c7793167865b9bdd122d3f55142e24201
Chase Whitney <ChaseEWhitney@gmail.com>
2019-03-13 16:31:48 -0500

    setup for production

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  cookieKey: process.env.COOKIE_KEY;
+  cookieKey: process.env.COOKIE_KEY,
+  mongoURI: process.env.MONGO_URI,
+  nutriKey: process.env.NUTRI_KEY,
+  nutriId: process.env.NUTRI_ID,
 };
