57f2ce351e4d34f3d62ee4c6cde02b816fc1f505
Chase <ChaseEWhitney@gmail.com>
2019-12-02 16:44:24 -0600

    landing page styling tweak

--- client/src/components/Landing/Landing.jsx.js
+++ client/src/components/Landing/Landing.jsx.js
@@ -4,15 +4,10 @@ export const LandingContainer = styled.div`
   font-size: 6rem;
   width: 100%;
   padding: 0.5rem;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
 
   & div {
     width: 50%;
     text-align: center;
-    margin-top: -10rem;
+    margin: 17rem auto;
   }
 `;
